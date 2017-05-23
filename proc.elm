83517dd99fe6544df262167f946e7c25d1965e19
Reggy G <rgranovskiy@gmail.com>
2017-05-23 13:29:27 -0400

    Added framework for ports?

--- proc.elm
+++ proc.elm
@@ -1,11 +1,20 @@
+port module Proc exposing (..)
+
 import Html
 import Html.Attributes
 import Html.Events
 import Http
 import Dict exposing (..)
-import FileReader
 
-main = Html.beginnerProgram {model=model, view=view, update=update}
+
+
+main = 
+    Html.program 
+        {init=init
+        , view=view
+        , update=update
+        , subscriptions=subscriptions
+        }
 
 
 ---MODEL---
@@ -33,38 +42,47 @@ type Msg =
     ViewChange View
     | Upload String
 
-update: Msg -> Model -> Model
+--port getImage : String -> Cmd msg 
+
+update: Msg -> Model -> (Model, Cmd msg)
 update msg model =
     case msg of
         ViewChange v ->
             case v of
                 Label ->
-                    {model|content = "A label"}
+                    ({model|content = "A label"}, Cmd.none)
                 Text -> 
-                    {model|content = "Some text"}
+                    ({model|content = "Some text"}, Cmd.none)
                 Face ->
-                    {model|content = "A face"}
+                    ({model|content = "A face"}, Cmd.none)
                 Wait ->
-                    {model|content = "Waiting"}
+                    ({model|content = "Waiting"}, Cmd.none)
         Upload s ->
-            {model|img = Picture s "stuff"}
+            ({model|img = Picture s "stuff"}, Cmd.none)
 
 
 ---VIEW---
 
 view model =
-    Html.div [Html.Attributes.class "meow"]
-    [Html.div [Html.Attributes.class "container"]  
-    [Html.img [Html.Attributes.class "image", Html.Attributes.src "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx", Html.Attributes.alt "Valid image required", Html.Attributes.title "Placeholder image"] [],
-    Html.div [] [],
-    Html.button [Html.Attributes.class "choosebutt", Html.Events.onClick (Upload "file")] [Html.text "Choose file"],
-    Html.div [] [],
-    Html.div [Html.Attributes.class "head"] [
-    headerButton "Label" (ViewChange Label) model, 
-    headerButton "Text" (ViewChange Text) model,
-    headerButton "Face" (ViewChange Face) model],
-    Html.div [Html.Attributes.class "body"]  [Html.text model.content],
-    Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "style.css" ] []]]
+    let viewImg =
+            case model.img of
+                Waiting ->
+                    Html.text ""
+                Picture f n ->
+                    Html.img [Html.Attributes.class "image", Html.Attributes.src f, Html.Attributes.title n] []
+    in
+        Html.div [Html.Attributes.class "meow"]
+        [Html.div [Html.Attributes.class "container"]  
+        [viewImg,
+        Html.div [] [],
+        Html.input [Html.Attributes.type_ "file", Html.Attributes.class "choosebutt"] [],
+        Html.div [] [],
+        Html.div [Html.Attributes.class "head"] [
+        headerButton "Label" (ViewChange Label) model, 
+        headerButton "Text" (ViewChange Text) model,
+        headerButton "Face" (ViewChange Face) model],
+        Html.div [Html.Attributes.class "body"]  [Html.text model.content],
+        Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "style.css" ] []]]
 
 
 ---HTML
@@ -73,6 +91,15 @@ headerButton: String->Msg->Model->Html.Html Msg
 headerButton s m model=
     Html.button [Html.Attributes.class "headbutt", Html.Events.onClick (m)] [Html.text s]
 
---INIT--
+--SUBSCRIPTIONS
 
-model = Model (Waiting) (Wait) "There will be attribute descriptions or a prompt for an image here someday" "" Dict.empty
+--port jsonresponse : (String -> msg) -> Sub msg
+
+subscriptions: Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+--INIT--
+init: (Model, Cmd msg)
+init = (Model (Waiting) (Wait) "There will be attribute descriptions or a prompt for an image here someday" "" Dict.empty, Cmd.none)
