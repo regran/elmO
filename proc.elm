a765eda6eb8a4b61e28b97de57946e6512fa8773
Reggy G <rgranovskiy@gmail.com>
2017-05-25 14:11:16 -0400

    Fixed styling of file input button using a label; implemented javascript interop to upload an image file and display the image; fixed margin

--- proc.elm
+++ proc.elm
@@ -5,7 +5,7 @@ import Html.Attributes
 import Html.Events
 import Http
 import Dict exposing (..)
-
+import Json.Decode as JD
 
 
 main = 
@@ -19,7 +19,7 @@ main =
 
 ---MODEL---
 type alias Model = 
-    {img: Image,
+    {img: Maybe Image,
     view: View,
     content: String,
     desc: String,
@@ -27,9 +27,10 @@ type alias Model =
     }
                 
                 
-type Image =
-    Waiting
-    | Picture String String
+type alias Image =
+    {contents: String,
+    name: String
+    }
 
 type View =
     Label
@@ -40,9 +41,10 @@ type View =
 ---UPDATE
 type Msg =
     ViewChange View
-    | Upload String
+    | Get
+    | Upload Image
 
---port getImage : String -> Cmd msg 
+port getImage : String -> Cmd msg 
 
 update: Msg -> Model -> (Model, Cmd msg)
 update msg model =
@@ -57,8 +59,10 @@ update msg model =
                     ({model|content = "A face"}, Cmd.none)
                 Wait ->
                     ({model|content = "Waiting"}, Cmd.none)
-        Upload s ->
-            ({model|img = Picture s "stuff"}, Cmd.none)
+        Get ->
+            (model, getImage "file")
+        Upload data ->
+            ({model|img = Just (Image data.contents data.name)}, Cmd.none)
 
 
 ---VIEW---
@@ -66,16 +70,17 @@ update msg model =
 view model =
     let viewImg =
             case model.img of
-                Waiting ->
+                Nothing ->
                     Html.text ""
-                Picture f n ->
-                    Html.img [Html.Attributes.class "image", Html.Attributes.src f, Html.Attributes.title n] []
+                Just i->
+                    Html.img [Html.Attributes.class "image", Html.Attributes.src i.contents, Html.Attributes.title i.name ] []
     in
         Html.div [Html.Attributes.class "meow"]
         [Html.div [Html.Attributes.class "container"]  
         [viewImg,
         Html.div [] [],
-        Html.input [Html.Attributes.type_ "file", Html.Attributes.class "choosebutt"] [],
+        Html.input [Html.Attributes.type_ "file", Html.Attributes.id "file", Html.Attributes.hidden True, Html.Events.on "change" (JD.succeed Get)] [],
+        Html.label [Html.Attributes.for "file", Html.Events.on "change" (JD.succeed Get), Html.Attributes.class "choosebutt"] [Html.text "Choose File"],
         Html.div [] [],
         Html.div [Html.Attributes.class "head"] [
         headerButton "Label" (ViewChange Label) model, 
@@ -93,13 +98,13 @@ headerButton s m model=
 
 --SUBSCRIPTIONS
 
+port fileContentRead : (Image->msg) -> Sub msg
 --port jsonresponse : (String -> msg) -> Sub msg
 
 subscriptions: Model -> Sub Msg
 subscriptions model =
-    Sub.none
-
+    fileContentRead Upload
 
 --INIT--
 init: (Model, Cmd msg)
-init = (Model (Waiting) (Wait) "There will be attribute descriptions or a prompt for an image here someday" "" Dict.empty, Cmd.none)
+init = (Model (Nothing) (Wait) "There will be attribute descriptions or a prompt for an image here someday" "" Dict.empty, Cmd.none)
